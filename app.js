49e9f12bcff4a2a6ea6a2c6e4598c903184926cd
ZAIN JAMI <zainjami427@gmail.com>
2025-02-17 10:42:35 +0500

    exercise

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Sir Abdullah");
+console.log(1234);
+alert("SirAbdullah");
+console.log(10+10)
+console.log(20-10)
+console.log(12+"10")
+console.log('jami')
+// String{"jami writes"};
+// Number(1234);
+// Boolean true/false
