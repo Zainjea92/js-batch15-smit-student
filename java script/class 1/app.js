28a418ce085bdbe843200b1c302f0c351f3f13d6
ZAIN JAMI <zainjami427@gmail.com>
2025-02-21 16:07:08 +0500

    Add files via upload

---
+++ java script/class 1/app.js
@@ -0,0 +1,50 @@
+// console.log("Zain jam");
+// console.log(1234);
+// console.log(true);
+
+// var username = prompt ("what is your name");
+// var email = prompt  ("what is your email");
+// var age = prompt ("what is your age");
+// var city = prompt  ("what is your city");
+
+// console.log("username->",username) //    username->     zain
+// console.log("email->",email)
+// console.log("age->",age)
+// console.log("city->",city)
+
+
+
+// var username = prompt ("enter your name");
+// var city = prompt ("enter your city");
+// var age = prompt("enter your age");
+// var email= prompt("inter your email");
+
+// console.log(username);
+// console.log(city);
+// console.log(age);
+// console.log(email);
+
+
+
+
+// var totalmraks = 400;
+
+// var username = prompt("enter your name ");
+// var urdumarks = +prompt("enter your urdumarks");
+// var mathsmarks = +prompt("enter your mathsmarks");
+// var engmarks = +prompt("enter your engmarks");
+// var commarks = +prompt("enter your compmarks");
+
+
+// var obtaintMarks = urdumarks + mathsmarks + engmarks + commarks;
+
+// var percentage = obtaintMarks / totalmraks * 100;
+
+// console.log(percentage)
+
+
+
+
+
+
+
